db8b709a9ca7217272bd360b935854b9b25f40a8
k-mcneillie <110234880+k-mcneillie@users.noreply.github.com>
2026-08-08 22:56:34 +0000

    test: analytical working example

--- experiments/scripts/baseline.py
+++ experiments/scripts/baseline.py
@@ -1,7 +1,20 @@
+
+from pinn_heat_1d.analytical import analytical_solution
+from pinn_heat_1d.data.utils import load_dataset
 from pinn_heat_1d.utils.session import Session
 
-session = Session("baseline")
+from pathlib import Path
+
+session = Session("test-analytical-method", seed=42, device="cpu")
 
 logger = session.logger
 
-logger.info("Starting PINN training.")
+dataset_path = Path("outputs/20260808_215130_test-dataset-gen/data.pt")
+
+data = load_dataset(dataset_path)
+logger.info("Dataset loaded...")
+
+x = data.initial[:, 0]
+t = data.initial[:, 1]
+
+u_exact = analytical_solution(x, t)
